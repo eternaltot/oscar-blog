7b4c4bb480d9fe595a3140576a71cf294b1c2d14
Puchong Intusmit <eternaltot@gmail.com>
2019-01-25 19:57:39 +0700

    change model to abstract model

--- blogs/models.py
+++ blogs/models.py
@@ -3,9 +3,25 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+        
 class Post(models.Model):
-    blog_title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    post_title = models.CharField(max_length=200)
+    post_content = models.TextField()
+    feature_image = models.ImageField()
+    post_date = models.DateTimeField('Post Date')
+    categories = models.ManyToManyField(
+        Category,
+        through='CategoryMapping',
+        through_fields=('post', 'category'),
+    )
+    post_excerpt = models.CharField(max_length=200)
 
     def __str__(self):
         return self.question_text
@@ -18,13 +34,7 @@ class Post(models.Model):
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-
-class Category(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
-
-class Postmeta(models.Model)
+class CategoryMapping(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
